000203c8060f12d987805004a076a48897b878b8
Li Kai <falconets25601@gmail.com>
2018-04-16 17:09:50 +0800

    Enable parallel playing

--- src/PlayerSkeleton.java
+++ src/PlayerSkeleton.java
@@ -1,4 +1,9 @@
+import java.util.OptionalDouble;
+import java.util.stream.IntStream;
+
 public class PlayerSkeleton {
+
+    private static boolean SHOW_WINDOW = false;
 
     //implement this function to have a working system
     public int pickMove(State s, int[][] legalMoves) {
@@ -7,8 +12,14 @@ public class PlayerSkeleton {
     }
 
     public static void main(String[] args) {
+        PlayerSkeleton.playMultiple(5);
+    }
+
+    public static void play() {
         State s = new State();
-        new TFrame(s);
+        if (PlayerSkeleton.SHOW_WINDOW) {
+            new TFrame(s);
+        }
         PlayerSkeleton p = new PlayerSkeleton();
         while (!s.hasLost()) {
             s.makeMove(p.pickMove(s, s.legalMoves()));
@@ -21,6 +32,22 @@ public class PlayerSkeleton {
             }
         }
         System.out.println("You have completed " + s.getRowsCleared() + " rows.");
+    }
+
+    public static void playMultiple(int n) {
+        OptionalDouble avgScore = IntStream.range(0, n).parallel().map(i -> {
+            State s = new State();
+            if (PlayerSkeleton.SHOW_WINDOW) {
+                new TFrame(s);
+            }
+            PlayerSkeleton p = new PlayerSkeleton();
+            while (!s.hasLost()) {
+                s.makeMove(p.pickMove(s, s.legalMoves()));
+            }
+            return s.getRowsCleared();
+        }).average();
+
+        System.out.println("You have completed " + avgScore.getAsDouble() + " rows.");
     }
 
 }
